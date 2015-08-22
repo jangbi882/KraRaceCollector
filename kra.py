1625a8eb9fde2ad6fe4ecf82be0e74724ebcc459
jangbi <jangbi882@gmail.com>
2015-08-22 22:07:17 +0900

    최근10회전적까지 완료

--- kra.py
+++ kra.py
@@ -53,20 +53,19 @@ option_data = {
         "split_column_list" : [(2, r"(\d+)\((\d+)\/(\d+)\)", 0), (3, r"(.*)\%", 0), (4, r"(.*)\%", 0)
             ,(5, r"(\d+\:\d+.\d+).+", 0), (6, r"(\d+\:\d+.\d+).+", 0), (7, r"(\d+\:\d+.\d+).+", 0)]
     },
-    # "near10_rec": {
-    #     "desc" : u"최근10회전적",
-    #     "url": 'http://race.kra.co.kr/chulmainfo/chulmaDetailInfo10Score.do',
-    #     "values" : {
-    #         "rcDate": '{date}',
-    #         "rcNo": '{race_no}',
-    #         "Sub":"1",
-    #         "Act":"02",
-    #         "meet":"1"
-    #     },
-    #     "data_table_no" : 2,
-    #     "split_column_list" : [(10, r"(\d+\:\d+.\d+).+", 0), (11, r"(\d+\:\d+.\d+).+", 0), (12, r"(\d+\:\d+.\d+).+", 0),
-    #                            (13, r"(\d+\:\d+.\d+).+", 0), (14, r"(\d+)\((\d+)\)", 0)]
-    # },
+    "near10_rec": {
+        "desc" : u"최근10회전적",
+        "url": 'http://race.kra.co.kr/chulmainfo/chulmaDetailInfo10Score.do',
+        "values" : {
+            "rcDate": '{date}',
+            "rcNo": '{race_no}',
+            "Sub":"1",
+            "Act":"02",
+            "meet":"1"
+        },
+        "data_table_no" : 2,
+        "split_column_list" : [(14, r"(.+)\((.+)\)", 0)]
+    },
 }
 
 def get_table(option, date, race_no, table_no):
@@ -108,6 +107,12 @@ def get_table(option, date, race_no, table_no):
         # 여기서 인코딩을 지정해 주어야 한다.
         soup = BeautifulSoup(page, "lxml", from_encoding='cp949')
 
+        ######
+        # 파일이름 정하기
+        filename = u"{}_race{}_{}.txt".format(date, race_no, option)
+
+        ######
+        # 대상 테이블 찾기
         try:
             i_table_no = int(table_no) - 1
         except Exception:
@@ -122,108 +127,122 @@ def get_table(option, date, race_no, table_no):
         elif i_table_no < 0:
             i_table_no = 0
 
-        table_no = i_table_no+1
-        table = all_table[i_table_no]
-
-        if option == "score" :
-            caption = table.find("caption")
-            if caption:
-                filename = u"{}_race{}{}_{}.txt".format(date, race_no, u"_table{}".format(table_no) if table_no else "",
-                                                      caption.contents[0])
-            else:
-                filename = u"{}_race{}{}.txt".format(date, race_no, u"_table{}".format(table_no) if table_no else "")
-        else:
-            filename = u"{}_race{}_{}.txt".format(date, race_no, option)
-
-        tr_rows = []
-        for tr in table.find_all('tr'):
-            ths = tr.select("th")
-            if len(ths) > 0:
-                tr_rows.append(tr)
-        if len(tr_rows) == 2:
-            headers = []
-            tr1 = tr_rows[0].select("th")
-            tr2 = tr_rows[1].select("th")
-            tr2_ptr = 0
-            for th1 in tr1:
-                try:
-                    rowspan = int(th1['rowspan'])
-                except KeyError:
-                    rowspan = 1
-
-                try:
-                    colspan = int(th1['colspan'])
-                except KeyError:
-                    colspan = 1
-
-                th1_str = th1.text.encode("utf-8").strip()
-                if colspan == 1:
-                    headers.append(th1_str)
-                else:
-                    for i in range(tr2_ptr, tr2_ptr+colspan):
-                        th2 = tr2[i]
-                        th2_str = th2.text.encode("utf-8").strip()
-                        headers.append(th1_str+"_"+th2_str)
-                    tr2_ptr += colspan
-
-        else:
-            headers = [header.text.encode("utf-8").strip() for header in table.find_all('tr th[colspan,rowspan]')]
-
-        # 둘로 나눠야 하는 컬럼 추가
-        if split_column_list:
-            split_column_list.sort(reverse=True)
-            for logic in split_column_list:
-                col_no = logic[0]
-                r = re.compile(logic[1])
-                num_cal = r.groups
-                org_col_name = headers[col_no]
-                headers[col_no] = org_col_name+"_1"
-                for n in range(1, num_cal):
-                    headers.insert(col_no+n, org_col_name+"_{}".format(n+1))
-
+        headers = []
         rows = []
-        for row in table.find_all('tr'):
-            cols = row.find_all('td')
-            col_data =[re.sub(u"\s+", " ", val.text.encode("utf-8").strip().replace("\n", " ")) for val in cols]
-            if len(cols) <= 0:
-                continue
-            if split_column_list:
-                # 여러 컬럼으로 분라하거나 특수문자 제거
-                for logic in split_column_list:
-                    col_no = logic[0]
-                    r = re.compile(logic[1])
-                    num_cal = r.groups
-                    def_cal = logic[2]
-                    org_val = col_data[col_no]
-                    del col_data[col_no]
+        for i_table_no in range(i_table_no, (i_table_no)+1 if option != "near10_rec" else len(all_table)):
 
-                    match = r.search(org_val)
-                    if match:
-                        vals = match.groups()
-                        vals = reversed(vals)
-                        for val in vals:
-                            col_data.insert(col_no, val)
-                    else:
-                        for i in range(num_cal-1, -1 , -1):
-                            if i == def_cal:
-                                col_data.insert(col_no, org_val)
-                            else:
-                                col_data.insert(col_no, '')
+            table = all_table[i_table_no]
 
-            # 시간을 변환하여야 하는 것이 있는지 확인
-            for i, val in enumerate(col_data):
-                res = time_re.search(val)
-                if res:
-                    col_data[i] = str(int(res.group(1))*60 + float(res.group(2)+"."+res.group(3)))
+            ######
+            tr_rows = []
 
-            rows.append(col_data)
+            # 컬럼 헤더 만들기
+            for tr in table.find_all('tr'):
+                ths = tr.select("th")
+                if len(ths) > 0:
+                    tr_rows.append(tr)
 
-        csv = "|".join(headers)
+            # 최근10회전적 예외 처리
+            horse_name = None
+            if option == "near10_rec":
+                anchor = tr_rows[0].select("a")[0]
+                horse_name = anchor.text.encode("utf-8").strip()
+                del tr_rows[0]
+
+            if len(headers) <= 0:
+                if len(tr_rows) == 1:
+                    headers = [header.text.encode("utf-8").strip() for header in tr_rows[0].select("th")]
+                elif len(tr_rows) == 2:
+                    tr1 = tr_rows[0].select("th")
+                    tr2 = tr_rows[1].select("th")
+                    tr2_ptr = 0
+                    for th1 in tr1:
+                        try:
+                            rowspan = int(th1['rowspan'])
+                        except KeyError:
+                            rowspan = 1
+
+                        try:
+                            colspan = int(th1['colspan'])
+                        except KeyError:
+                            colspan = 1
+
+                        th1_str = th1.text.encode("utf-8").strip()
+                        if colspan == 1:
+                            headers.append(th1_str)
+                        else:
+                            for i in range(tr2_ptr, tr2_ptr+colspan):
+                                th2 = tr2[i]
+                                th2_str = th2.text.encode("utf-8").strip()
+                                headers.append(th1_str+"_"+th2_str)
+                            tr2_ptr += colspan
+
+                # 둘로 나눠야 하는 컬럼 추가
+                if split_column_list:
+                    split_column_list.sort(reverse=True)
+                    for logic in split_column_list:
+                        col_no = logic[0]
+                        r = re.compile(logic[1])
+                        num_cal = r.groups
+                        org_col_name = headers[col_no]
+                        headers[col_no] = org_col_name+"_1"
+                        for n in range(1, num_cal):
+                            headers.insert(col_no+n, org_col_name+"_{}".format(n+1))
+
+                # 최근10회전적 예외처리
+                if horse_name:
+                    headers.insert(0, "마명")
+
+            ######
+            # 데이터 넣기
+            for row in table.find_all('tr'):
+                cols = row.find_all('td')
+                col_data =[re.sub(u"\s+", " ", val.text.encode("utf-8").strip().replace("\n", " ")) for val in cols]
+                if len(cols) <= 0:
+                    continue
+                if split_column_list:
+                    # 여러 컬럼으로 분라하거나 특수문자 제거
+                    for logic in split_column_list:
+                        col_no = logic[0]
+                        r = re.compile(logic[1])
+                        num_cal = r.groups
+                        def_cal = logic[2]
+                        org_val = col_data[col_no]
+                        del col_data[col_no]
+
+                        match = r.search(org_val)
+                        if match:
+                            vals = match.groups()
+                            vals = reversed(vals)
+                            for val in vals:
+                                col_data.insert(col_no, val)
+                        else:
+                            for i in range(num_cal-1, -1 , -1):
+                                if i == def_cal:
+                                    col_data.insert(col_no, org_val)
+                                else:
+                                    col_data.insert(col_no, '')
+
+                # 시간을 변환하여야 하는 것이 있는지 확인
+                for i, val in enumerate(col_data):
+                    res = time_re.search(val)
+                    if res:
+                        col_data[i] = str(int(res.group(1))*60 + float(res.group(2)+"."+res.group(3)))
+
+                # 최근10회전적 예외처리
+                if horse_name:
+                    col_data.insert(0, horse_name)
+
+                rows.append(col_data)
+
+        csv = ",".join(headers)
         csv += "\n"
 
+        ######
+        # 파일에 쓰기
         for row in rows:
             if len(row) > 0:
-                csv += "|".join(row)
+                csv += ",".join(row)
                 csv += "\n"
 
         with open(filename, "w") as out_file:
@@ -275,7 +294,7 @@ if __name__ == '__main__':
     else:
         table_no = None
     '''
-    option = "course_rec"
+    option = "near10_rec"
     date = "20150822"
     race_no = "1"
     table_no = None
