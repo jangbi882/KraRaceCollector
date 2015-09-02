e431eb5a025e58dbf33d61f8430c8bebeba2b85b
jangbi <jangbi882@gmail.com>
2015-09-02 22:25:56 +0900

    지역선택 추가, 데이터 잘리는 문제 수정, 요청헤더 강화

--- kra.py
+++ kra.py
@@ -6,6 +6,7 @@ import re
 import sys
 import locale
 import os.path
+import time
 
 os_encoding = locale.getpreferredencoding()
 file_base = os.path.basename(__file__)
@@ -20,7 +21,7 @@ option_data = {
             "rcNo": '{race_no}',
             "Sub":"1",
             "Act":"02",
-            "meet":"1"
+            "meet": '{city}'
         },
         "data_table_no" : 2,
         "split_column_list" : [(6, r"(\d+)\((\d+)\)", 0), (7, r"\((.*)\)(.*)", 1)]
@@ -33,7 +34,7 @@ option_data = {
             "rcNo": '{race_no}',
             "Sub":"1",
             "Act":"02",
-            "meet":"1"
+            "meet": '{city}'
         },
         "data_table_no" : 2,
         "split_column_list" : [(2, r"(\d+)\((\d+)\/(\d+)\)", 0), (3, r"(.*)\%", 0), (4, r"(.*)\%", 0)
@@ -47,7 +48,7 @@ option_data = {
             "rcNo": '{race_no}',
             "Sub":"1",
             "Act":"02",
-            "meet":"1"
+            "meet": '{city}'
         },
         "data_table_no" : 2,
         "split_column_list" : [(2, r"(\d+)\((\d+)\/(\d+)\)", 0), (3, r"(.*)\%", 0), (4, r"(.*)\%", 0)
@@ -61,19 +62,24 @@ option_data = {
             "rcNo": '{race_no}',
             "Sub":"1",
             "Act":"02",
-            "meet":"1"
+            "meet": '{city}'
         },
         "data_table_no" : 2,
         "split_column_list" : [(14, r"(.+)\((.+)\)", 0)]
     },
 }
 
-def get_table(option, date, race_no, table_no):
+def get_table(option, date, race_no, table_no, city):
 
     try:
         info = option_data[option]
     except KeyError:
         return None
+
+    if city == "seoul": i_city = 1
+    elif city == "jeju" : i_city = 2
+    elif city == "busan" : i_city = 3
+    else: i_city = 1
 
     url = info["url"]
     values = info["values"]
@@ -81,6 +87,7 @@ def get_table(option, date, race_no, table_no):
         val = values[key]
         if val == '{date}': values[key] = date
         elif val == '{race_no}' : values[key] = race_no
+        elif val == '{city}' : values[key] = i_city
 
     # 데이터가 있는 테이블 번호; 0에서 시작
     data_table_no = info["data_table_no"]
@@ -88,44 +95,78 @@ def get_table(option, date, race_no, table_no):
     # (col_no, pattern, def_col)
     split_column_list = info["split_column_list"]
 
+    # 해더를 잘 적어 주지 않으면 시스템이 비정상 호출로 거부 당한다.
     headers = {
-        "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
+        "Accept-Encoding" : "gzip, deflate, sdch"
+        ,"Accept-Language" : "ko,en-US;q=0.8,en;q=0.6"
+        ,"Upgrade-Insecure-Requests" : "1"
+        ,"User-Agent" : "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+        ,"Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+        ,"Referer" : url
+        # 쿠키는 변경된 가능성을 확인해야 한다.
+        ,"Cookie" : "WMONID=Nz7aZM1C3N0; NRACE_JSESSIONID=Of-NDgpECnNTA7a91UDqWQ7Nw-9Yvyy_Pp3PO1DsIFt9DAHhBzkO!-1826629772; itoken=null"
+        ,"Connection" : "keep-alive"
     }
     data = urllib.urlencode(values)
     try:
-        request = urllib2.Request(url, data, headers)
-        response = urllib2.urlopen(request)
-        # 마사회 페이지는 euc-kr
-        # page = response.read().decode('euc-kr', 'ignore')
-        # page = response.read().decode('cp949', 'ignore')
-        # 하지만 인코딩을 지정해 주면 오히려 깨진다.
-        page = response.read()
-        response.close()
+        # 데이터가 한 번에 나오지 않는 경우가 있어 NUM_RETRY회까지 재요청하게 구조 수정
+        NUM_RETRY = 20
 
-        # parser로 lxml을 사용. 별도 설치 필요. 더 빠르고 강력하다.
-        # soup = BeautifulSoup(page, "lxml")
-        # 여기서 인코딩을 지정해 주어야 한다.
-        soup = BeautifulSoup(page, "lxml", from_encoding='cp949')
+        for num_iter in range(NUM_RETRY):
+            request = urllib2.Request(url, data, headers)
+            response = urllib2.urlopen(request)
+            # 마사회 페이지는 euc-kr
+            # page = response.read().decode('euc-kr', 'ignore')
+            # page = response.read().decode('cp949', 'ignore')
+            # 하지만 인코딩을 지정해 주면 오히려 깨진다.
+            page = response.read()
+            response.close()
 
-        ######
-        # 파일이름 정하기
-        filename = u"{}_race{}_{}.txt".format(date, race_no, option)
+            with open("dump.html", "w") as out_file:
+                out_file.write(page)
 
-        ######
-        # 대상 테이블 찾기
-        try:
-            i_table_no = int(table_no) - 1
-        except Exception:
-            if data_table_no:
-                i_table_no = data_table_no;
-            else:
-                i_table_no = 0
+            # parser로 lxml을 사용. 별도 설치 필요. 더 빠르고 강력하다.
+            # soup = BeautifulSoup(page, "lxml")
+            # 여기서 인코딩을 지정해 주어야 한다.
 
-        all_table = soup.find_all("table")
-        if i_table_no >= len(all_table):
-            i_table_no = len(all_table) - 1
+            ## lxml 파서의 경우 html을 파싱하다 잘리는 경우가 많이 발견되어 html.parser로 변경
+            # soup = BeautifulSoup(page, "lxml", from_encoding='cp949')
+            soup = BeautifulSoup(page, 'html.parser', from_encoding='cp949')
+
+            ######
+            # 파일이름 정하기
+            filename = u"{0}_{1}_{2}_race{3:02d}.txt".format(city, option, date, int(race_no))
+
+            ######
+            # 대상 테이블 찾기
+            try:
+                i_table_no = int(table_no) - 1
+            except Exception:
+                if data_table_no:
+                    i_table_no = data_table_no
+                else:
+                    i_table_no = 0
+
+            all_table = soup.find_all("table")
+            num_all_table = len(all_table)
+
+            if num_all_table > 0 or num_iter+1 >= NUM_RETRY:
+                break
+
+            print ("Retrying...")
+            time.sleep(2)
+
+
+        if i_table_no >= num_all_table:
+            i_table_no = num_all_table - 1
         elif i_table_no < 0:
             i_table_no = 0
+
+        #결국 못 얻어온 경우
+        if i_table_no < 0:
+            print ("FAIL!!!")
+            return False
+
 
         headers = []
         rows = []
@@ -145,9 +186,12 @@ def get_table(option, date, race_no, table_no):
             # 최근10회전적 예외 처리
             horse_name = None
             if option == "near10_rec":
-                anchor = tr_rows[0].select("a")[0]
-                horse_name = anchor.text.encode("utf-8").strip()
-                del tr_rows[0]
+                if len(tr_rows) > 0:
+                    anchor = tr_rows[0].select("a")[0]
+                    horse_name = anchor.text.encode("utf-8").strip()
+                    del tr_rows[0]
+                else:
+                    horse_name = "Unknown"
 
             if len(headers) <= 0:
                 if len(tr_rows) == 1:
@@ -256,17 +300,18 @@ def get_table(option, date, race_no, table_no):
         print e.reason.args[1]
         return False
     except Exception as e:
-        print str(e)
-        return False
+        # print str(e)
+        # return False
+        raise e
+
 
     return filename
 
 
 #############
 def help(add_val):
-    print "USAGE   :\t{}{} <option> <date> <race_no> [<table_no>]".format("python " if add_val==1 else "", file_base)
-    print "EXAMPLE :\t{}{} seoul_parti 20150719 1".format("python " if add_val==1 else "", file_base)
-    print "EXAMPLE :\t{}{} scm 20150719 1".format("python " if add_val==1 else "", file_base)
+    print "USAGE   :\t{}{} <city> <option> <date> <race_no> [<table_no>]".format("python " if add_val==1 else "", file_base)
+    print "EXAMPLE :\t{}{} busan contestant 20150719 1".format("python " if add_val==1 else "", file_base)
     print
     print "\n== Option List =="
     for cmd in option_data.keys():
@@ -277,7 +322,6 @@ def help(add_val):
 if __name__ == '__main__':
     by_python = 0
 
-    '''
     if len(sys.argv) < 1:
         help(by_python)
     if sys.argv[0] == file_base:
@@ -286,25 +330,29 @@ if __name__ == '__main__':
     if len(sys.argv) < 3+by_python:
         help(by_python)
 
-    option = sys.argv[1]
-    date = sys.argv[2]
-    race_no = sys.argv[3]
-    if 4 < len(sys.argv):
-        table_no = sys.argv[4]
+    city = sys.argv[1]
+    option = sys.argv[2]
+    date = sys.argv[3]
+    race_no = sys.argv[4]
+    if 5 < len(sys.argv):
+        table_no = sys.argv[5]
     else:
         table_no = None
+
     '''
+    city = "busan"
     option = "near10_rec"
-    date = "20150822"
-    race_no = "1"
+    date = "20150904"
+    race_no = "5"
     table_no = None
+    '''
 
     command_list = option_data.keys()
     if option not in command_list:
         print "Invalid option: {}".format(option)
         help(by_python)
 
-    filename = get_table(option, date, race_no, table_no if option=="score" else None)
+    filename = get_table(option, date, race_no, table_no if option=="score" else None, city)
 
     if filename:
         print("Result file {} is created.".format(filename.encode(os_encoding)))
